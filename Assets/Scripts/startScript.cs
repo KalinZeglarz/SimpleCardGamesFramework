70c77e6cf27e9a02b270808aaa2bf23d2d89dc9e
Konrad Marciniak <kondziooo24@gmail.com>
2020-06-25 09:20:13 +0200

    initial

---
+++ Assets/Scripts/startScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class startScript : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public Text theText;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        theText.color = Color.yellow;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        theText.color = Color.black;
+    }
+}
