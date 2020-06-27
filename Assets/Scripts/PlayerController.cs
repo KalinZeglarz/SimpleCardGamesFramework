514a1b5bd4d7777360077fe2c272d8335543f39d
Konrad Marciniak <kondziooo24@gmail.com>
2020-06-27 14:40:25 +0200

    gui edit 2

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(0, speed, 0);
+        }  
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(0, -speed, 0);
+        }  
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(speed, 0, 0);
+        }  
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(-speed, 0, 0);
+        }  
+    }
+}
